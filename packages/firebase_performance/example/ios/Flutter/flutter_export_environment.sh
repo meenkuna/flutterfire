1e3d92c38492d1dbf8bd6a6dbdab0c8e5888d0a0
meenkuna <koiking.jpg@gmail.com>
2021-06-28 15:02:08 +0700

    flutter

--- packages/firebase_performance/example/ios/Flutter/flutter_export_environment.sh
+++ packages/firebase_performance/example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\API\flutterfire\packages\firebase_in_app_messaging\example"
+export "FLUTTER_APPLICATION_PATH=D:\API\flutterfire\packages\firebase_performance\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
---
+++ packages/firebase_performance/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,35 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<e2e/E2EPlugin.h>)
+#import <e2e/E2EPlugin.h>
+#else
+@import e2e;
+#endif
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<firebase_performance/FLTFirebasePerformancePlugin.h>)
+#import <firebase_performance/FLTFirebasePerformancePlugin.h>
+#else
+@import firebase_performance;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [E2EPlugin registerWithRegistrar:[registry registrarForPlugin:@"E2EPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebasePerformancePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebasePerformancePlugin"]];
+}
+
+@end
